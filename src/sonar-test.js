f5a67067bdf061852b42f17749992cd4b11da5e4
dimitri.anoudis <dimitri.anoudis@news.co.uk>
2025-04-10 13:17:25 +0100

    lint fix

--- src/sonar-test.js
+++ src/sonar-test.js
@@ -1,21 +1,20 @@
 function calculateSum(arr) {
-  return arr.reduce((total, value) => total + value, 0);
+  return arr.reduce((total, value) => total + value, 0)
 }
 
 function computeValue(n) {
   if (n > 10) {
-
     const numbers = Array.from({ length: n }, (_, i) => {
       if (i % 2 === 0) {
-        return i;
+        return i
       } else if (i % 3 === 0) {
-        return -i;
+        return -i
       }
-      return i / 2;
-    });
-    return calculateSum(numbers);
+      return i / 2
+    })
+    return calculateSum(numbers)
   }
-  return n * 2;
+  return n * 2
 }
 
 export { calculateSum, computeValue }
