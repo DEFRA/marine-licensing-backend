b64f79712fa77853841763b5bd8fba7f623ffd21
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-05-08 14:53:49 +0100

    Patch routes for Project Name and Public Register

---
+++ src/api/exemptions/controllers/update-public-register.test.js
@@ -0,0 +1,67 @@
+import { ObjectId } from 'mongodb'
+import { updatePublicRegisterController } from './update-public-register.js'
+
+describe('PATCH /exemptions/public-register', () => {
+  const payloadValidator =
+    updatePublicRegisterController.options.validate.payload
+
+  it('should fail if fields are missing', () => {
+    const result = payloadValidator.validate({})
+
+    expect(result.error.message).toContain('PUBLIC_REGISTER_CONSENT_REQUIRED')
+  })
+
+  it('should fail if consent is empty string', () => {
+    const result = payloadValidator.validate({
+      consent: ''
+    })
+
+    expect(result.error.message).toContain('PUBLIC_REGISTER_CONSENT_REQUIRED')
+  })
+
+  it('should update exemption with public register', async () => {
+    const { mockMongo, mockHandler } = global
+
+    jest.spyOn(mockMongo, 'collection').mockImplementation(() => {
+      return {
+        updateOne: jest.fn().mockResolvedValueOnce({})
+      }
+    })
+
+    await updatePublicRegisterController.handler(
+      {
+        db: mockMongo,
+        payload: { id: new ObjectId().toHexString(), consent: false }
+      },
+      mockHandler
+    )
+
+    expect(mockHandler.response).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: 'success'
+      })
+    )
+  })
+
+  it('should return an error message if the database operation fails', async () => {
+    const { mockMongo, mockHandler } = global
+
+    const mockError = 'Database failed'
+
+    jest.spyOn(mockMongo, 'collection').mockImplementation(() => {
+      return {
+        updateOne: jest.fn().mockRejectedValueOnce(new Error(mockError))
+      }
+    })
+
+    expect(() =>
+      updatePublicRegisterController.handler(
+        {
+          db: mockMongo,
+          payload: { id: new ObjectId().toHexString(), consent: false }
+        },
+        mockHandler
+      )
+    ).rejects.toThrow(`Error updating public register: ${mockError}`)
+  })
+})
---
+++ src/models/public-register.js
@@ -0,0 +1,27 @@
+import joi from 'joi'
+import { exemptionId } from './shared-models.js'
+
+const PUBLIC_REGISTER_REASON_MAX_TEXT_LENGTH = 1000
+
+export const publicRegister = joi
+  .object({
+    consent: joi.boolean().required().messages({
+      'boolean.base': 'PUBLIC_REGISTER_CONSENT_REQUIRED',
+      'any.required': 'PUBLIC_REGISTER_CONSENT_REQUIRED'
+    }),
+    reason: joi.when('consent', {
+      is: true,
+      then: joi
+        .string()
+        .required()
+        .min(1)
+        .max(PUBLIC_REGISTER_REASON_MAX_TEXT_LENGTH)
+        .messages({
+          'string.empty': 'PUBLIC_REGISTER_REASON_REQUIRED',
+          'string.max': 'PUBLIC_REGISTER_REASON_MAX_LENGTH',
+          'any.required': 'PUBLIC_REGISTER_REASON_REQUIRED'
+        }),
+      otherwise: joi.forbidden()
+    })
+  })
+  .append(exemptionId)
