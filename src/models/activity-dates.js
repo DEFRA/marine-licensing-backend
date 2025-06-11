f9092f3490c24b9622bdb0b1d906df86712457ab
roppa <mark.robson@esynergy.co.uk>
2025-06-11 11:26:24 +0100

    fix: number literals. Matching error messages with fe

--- src/models/activity-dates.js
+++ src/models/activity-dates.js
@@ -8,7 +8,10 @@ const MAX_YEAR = MIN_YEAR + MAX_YEAR_OFFSET
 const MIN_DATE = new Date()
 MIN_DATE.setHours(0, 0, 0, 0)
 
-const MAX_DATE = new Date(MAX_YEAR, 11, 31)
+const MAX_DAYS_IN_MONTH = 31
+const MAX_MONTHS_IN_YEAR = 12
+
+const MAX_DATE = new Date(MAX_YEAR, MAX_MONTHS_IN_YEAR, MAX_DAYS_IN_MONTH)
 
 export const activityDatesSchema = joi
   .object()
@@ -19,16 +22,12 @@ export const activityDatesSchema = joi
       'date.min': 'CUSTOM_START_DATE_TODAY_OR_FUTURE',
       'date.max': 'CUSTOM_START_DATE_TODAY_OR_FUTURE'
     }),
-    end: joi
-      .date()
-      .required()
-      .greater(joi.ref('start'))
-      .max(MAX_DATE)
-      .messages({
-        'any.required': 'CUSTOM_END_DATE_MISSING',
-        'date.base': 'CUSTOM_END_DATE_INVALID',
-        'date.greater': 'CUSTOM_END_DATE_BEFORE_START_DATE',
-        'date.max': 'CUSTOM_END_DATE_TOO_LATE'
-      })
+    end: joi.date().required().min(joi.ref('start')).max(MAX_DATE).messages({
+      'any.required': 'CUSTOM_END_DATE_MISSING',
+      'date.base': 'CUSTOM_END_DATE_INVALID',
+      'date.greater': 'CUSTOM_END_DATE_BEFORE_START_DATE',
+      'date.min': 'CUSTOM_END_DATE_BEFORE_START_DATE',
+      'date.max': 'CUSTOM_END_DATE_INVALID'
+    })
   })
   .append(exemptionId)
