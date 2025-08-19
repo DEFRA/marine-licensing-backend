57cb1c43e42a9120843b0b2ea3b82b115bd27f6d
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-08-19 17:30:28 +0100

    Fix sonar issue

--- src/models/site-details/site-name.js
+++ src/models/site-details/site-name.js
@@ -1,9 +1,11 @@
 import joi from 'joi'
 
+const SITE_NAME_MAX_WIDTH = 250
+
 export const siteNameFieldSchema = joi
   .string()
   .min(1)
-  .max(250)
+  .max(SITE_NAME_MAX_WIDTH)
   .required()
   .messages({
     'string.empty': 'SITE_NAME_REQUIRED',
