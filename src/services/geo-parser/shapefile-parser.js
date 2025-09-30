46338fffdcfd798e0d42cdc7ea1c9179c1cb5761
Jon Shipley <jon.shipley@esynergy.co.uk>
2025-09-30 17:27:19 +0100

    Convert CRS to WGS84 coordinates

--- src/services/geo-parser/shapefile-parser.js
+++ src/services/geo-parser/shapefile-parser.js
@@ -1,10 +1,11 @@
 import * as shapefile from 'shapefile'
-import { mkdtemp, rm, glob } from 'fs/promises'
+import { mkdtemp, rm, glob, readFile } from 'fs/promises'
 import { tmpdir } from 'os'
 import { join } from 'path'
 import AdmZip from 'adm-zip'
 import { createLogger } from '../../common/helpers/logging/logger.js'
 import * as path from 'node:path'
+import proj4 from 'proj4'
 
 const logger = createLogger()
 
@@ -16,6 +17,13 @@ const DEFAULT_OPTIONS = {
 
 /**
  * Shapefile parser service for zipped shapefiles
+ * Converts any CRS system understood by the `proj4` package to WGS84. If there
+ * isn't a prj file then it the CRS is unknown and the coordinates will be
+ * returned as-is.
+ *
+ * Edge case: multiple .prj files: the first will be used.
+ * Z coordinate elevation handling: ignored as out of scope.
+ *
  */
 export class ShapefileParser {
   constructor(options = {}) {
@@ -90,12 +98,115 @@ export class ShapefileParser {
   }
 
   /**
+   * Transform coordinates from an unknown CRS to WGS84 using a proj4 transform function
+   * *NB* Mutates input coordinates!
+   * @param {Array} coords - a pair of coordinates in [long, lat] format
+   * @param transformer - a proj4 transformer
+   */
+  transformCoordinates (coords, transformer) {
+    if (typeof coords[0] === 'number') {
+      // Single coordinate pair
+      const [x, y] = transformer.forward(coords)
+      coords[0] = x
+      coords[1] = y
+    } else {
+      // Nested array
+      coords.forEach(c => this.transformCoordinates(c, transformer))
+    }
+  }
+
+  /**
    * Parse a shapefile and return GeoJSON
    * @param {string} shpPath - Path to the .shp file
+   * @param {function} transformer - a proj4 CRS transformer to convert to WSG84. Null can also mean no conversion needed.
    * @returns {Promise<Object>} The parsed GeoJSON object
    */
-  async parseShapefile(shpPath) {
-    return shapefile.read(shpPath)
+  async parseShapefile(shpPath, transformer = null) {
+    const source = await shapefile.open(shpPath)
+    const features = []
+
+    let result
+    while (!(result = await source.read()).done) {
+      const feature = result.value
+
+      // Transform coordinates recursively
+      if (transformer !== null) {
+        this.transformCoordinates(feature.geometry.coordinates, transformer)
+      }
+      features.push(feature)
+    }
+
+    return {
+      type: 'FeatureCollection',
+      features
+    }
+  }
+
+  /**
+   * Find the *.prj file and return a path to it, if it exists, or null if it doesn't
+   * @param {string} directory where the Shapefile zip was extracted
+   * @returns {Promise<string|null>}
+   */
+  async findProjectionFile(directory) {
+    logger.debug({ directory }, 'Searching for projection file in directory')
+    try {
+      const files = await Array.fromAsync(
+        glob('**/*.[pP][rR][jJ]', {
+          cwd: directory
+        })
+      )
+      const paths = files.map((file) => path.join(directory, file))
+      if (paths.length === 0) {
+        return null
+      } else {
+        return paths[0]
+      }
+    } catch (error) {
+      logger.error(
+        { directory, error: error.message },
+        'Error during glob search for prj file'
+      )
+      return null
+    }
+  }
+
+  /**
+   * Return the content of the *.prj file or '' if it doesn't exist
+   * @param directory - the dir with individual *.shp and *.prj files
+   * @returns {Promise<string>}
+   */
+  async readProjectionFile(directory) {
+    const projFilePath = await this.findProjectionFile(directory)
+
+    let projText = ''
+    if (projFilePath !== null) {
+      // Read the .prj file to get the source CRS (Coordinate Reference System)
+      projText = await readFile(projFilePath, 'utf-8')
+    }
+    return projText
+  }
+
+  /**
+   * Create a Proj4 CRS transformer
+   * @param {string} projText - the content of the shapefile *.prj file or an empty string
+   * @returns {function|null}
+   */
+  createTransformer (projText) {
+    // Define target CRS (WGS84)
+    const targetCRS = 'EPSG:4326'
+
+    let transformer = null
+
+    if (!(projText.includes('GCS_WGS_1984') || projText.includes('GEOGCS["WGS 84"'))) {
+      // The source is not in WGS84 format - the coordinates will need converting to WGS84
+      try {
+        transformer = proj4(projText, targetCRS)
+      } catch (error) {
+        logger.error('shapefile-parser: createTransformer(): failed to create the transformer: ' + error.message)
+      }
+
+    }
+    return transformer
   }
 
   /**
@@ -110,6 +221,10 @@ export class ShapefileParser {
 
       try {
         const shapefiles = await this.findShapefiles(extractDir)
+        const projText = await this.readProjectionFile(extractDir)
+        const transformer = this.createTransformer(projText)
+        logger.debug({ projText }, 'Proj file content')
+        logger.debug({ transformer }, 'Transformer created')
 
         logger.debug(
           {
@@ -127,7 +242,7 @@ export class ShapefileParser {
 
         const allFeatures = []
         for (const shpPath of shapefiles) {
-          const geoJson = await this.parseShapefile(shpPath)
+          const geoJson = await this.parseShapefile(shpPath, transformer)
           allFeatures.push(...geoJson.features)
         }
 
