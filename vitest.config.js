6f4a527699efa3142b26957984b3e78a85023702
Jon Shipley <jon.shipley@esynergy.co.uk>
2025-12-09 09:25:05 +0000

    Add vitest coverage ignore  - for src/index.js

--- vitest.config.js
+++ vitest.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       reportsDirectory: './coverage',
       reporter: ['text', 'lcov'],
       include: ['src/**/*.js'],
-      exclude: [...configDefaults.exclude, 'coverage']
+      exclude: [...configDefaults.exclude, 'coverage', 'src/index.js']
     },
     setupFiles: ['.vite/mongo-memory-server.js', '.vite/setup-files.js']
   }
