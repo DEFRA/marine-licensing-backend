1e60e649cc920adde89131bbcc7b8783c7274986
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-01-09 17:21:55 +0000

    Remove duplicate function and move to general utils

--- src/common/helpers/geo/geo-utils.js
+++ src/common/helpers/geo/geo-utils.js
@@ -1,6 +1,7 @@
 import proj4 from 'proj4'
 import { buffer } from '@turf/turf'
 import Boom from '@hapi/boom'
+import { equal } from '../utils.js'
 
 export const singleOSGB36toWGS84 = ({ eastings, northings }) =>
   proj4('OSGB36', 'WGS84', [
@@ -17,10 +18,6 @@ export const addBufferToShape = (geometry, amount = 50) => {
       `Error adding buffer to shape: ${error.message}`
     )
   }
-}
-
-const equal = (x, y) => {
-  return Math.abs(x - y) < Number.EPSILON
 }
 
 export const areCoordsTheSame = (coord1, coord2) => {
