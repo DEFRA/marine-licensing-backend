0a129d4df83e6699a6593639084ebab2d3128391
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-01-20 09:27:10 +0000

    Move out update route to next story

--- src/api/marine-licenses/index.js
+++ src/api/marine-licenses/index.js
@@ -1,15 +1,9 @@
 import { createProjectNameController } from './controllers/create-project-name.js'
-import { updateProjectNameController } from './controllers/update-project-name.js'
 
 export const marineLicenses = [
   {
     method: 'POST',
     path: '/marine-license/project-name',
     ...createProjectNameController
-  },
-  {
-    method: 'PATCH',
-    path: '/marine-license/project-name',
-    ...updateProjectNameController
   }
 ]
