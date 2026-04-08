469a891b5728bbcc8c6910ba506512b1a6c765be
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-04-08 17:25:26 +0100

    Add activity details end point for site details

---
+++ src/marine-licences/api/controllers/add-activity-details.integration.test.js
@@ -0,0 +1,83 @@
+import { setupTestServer } from '../../../../tests/test-server.js'
+import { makePatchRequest } from '../../../../tests/server-requests.js'
+import { ObjectId } from 'mongodb'
+import { mockMarineLicence } from '../../models/test-fixtures.js'
+
+describe('PATCH /marine-licence/add-activity-details - integration tests', async () => {
+  const getServer = await setupTestServer()
+  const contactId = '123e4567-e89b-12d3-a456-426614174000'
+  const marineLicenceId = new ObjectId()
+
+  const mockSite = {
+    coordinatesType: 'manual'
+  }
+
+  const emptyActivityDetails = {
+    activityType: '',
+    activityDescription: '',
+    activityDuration: '',
+    completionDate: '',
+    activityMonths: '',
+    workingHours: ''
+  }
+
+  const buildPayload = (overrides = {}) => ({
+    id: marineLicenceId.toString(),
+    siteIndex: 0,
+    ...overrides
+  })
+
+  test('successfully adds activity details to the correct site', async () => {
+    const marineLicence = {
+      ...mockMarineLicence,
+      _id: marineLicenceId,
+      contactId,
+      siteDetails: [mockSite]
+    }
+
+    await globalThis.mockMongo
+      .collection('marine-licences')
+      .insertOne(marineLicence)
+
+    const { statusCode, body } = await makePatchRequest({
+      server: getServer(),
+      url: '/marine-licence/add-activity-details',
+      contactId,
+      payload: buildPayload()
+    })
+
+    expect(statusCode).toBe(200)
+    expect(body).toEqual({ message: 'success' })
+
+    const updated = await globalThis.mockMongo
+      .collection('marine-licences')
+      .findOne({ _id: marineLicenceId })
+
+    expect(updated.siteDetails[0].activityDetails).toEqual([
+      emptyActivityDetails
+    ])
+  })
+
+  test('returns 404 when siteIndex is out of range', async () => {
+    const siteId = new ObjectId()
+    const marineLicence = {
+      ...mockMarineLicence,
+      _id: siteId,
+      contactId,
+      siteDetails: [mockSite]
+    }
+
+    await globalThis.mockMongo
+      .collection('marine-licences')
+      .insertOne(marineLicence)
+
+    const { statusCode } = await makePatchRequest({
+      server: getServer(),
+      url: '/marine-licence/add-activity-details',
+      contactId,
+      payload: buildPayload({ id: siteId.toString(), siteIndex: 99 })
+    })
+
+    expect(statusCode).toBe(404)
+  })
+})
--- src/marine-licences/api/index.js
+++ src/marine-licences/api/index.js
@@ -6,6 +6,7 @@ import { submitMarineLicenceController } from './controllers/submit-marine-licen
 import { updateSpecialLegalPowersController } from './controllers/update-special-legal-powers.js'
 import { updateOtherAuthoritiesController } from './controllers/update-other-authorities.js'
 import { updateSiteDetailsController } from './controllers/update-site-details.js'
+import { addActivityDetailsController } from './controllers/add-activity-details.js'
 
 export const marineLicences = [
   {
@@ -52,5 +53,10 @@ export const marineLicences = [
     method: 'PATCH',
     path: '/marine-licence/other-authorities',
     ...updateOtherAuthoritiesController
+  },
+  {
+    method: 'PATCH',
+    path: '/marine-licence/add-activity-details',
+    ...addActivityDetailsController
   }
 ]
