c9181e723465e87b12591e45ebda4036f2d4c889
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-04-30 15:39:01 +0100

    Update import

--- src/marine-licences/models/activity-details/working-hours.js
+++ src/marine-licences/models/activity-details/working-hours.js
@@ -1,6 +1,5 @@
 import joi from 'joi'
 
-export const ACTIVITY_MIN_LENGTH = 1
 export const WORKING_HOURS_MAX_LENGTH = 1_000
 
 export const workingHoursSchema = joi
@@ -8,7 +7,6 @@ export const workingHoursSchema = joi
   .optional()
   .allow('')
   .trim()
-  .min(1)
   .max(WORKING_HOURS_MAX_LENGTH)
   .messages({
     'string.empty': 'WORKING_HOURS_REQUIRED',
