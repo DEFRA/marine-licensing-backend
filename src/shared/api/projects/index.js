b42ea634dac4b359df5d02e4520cd6d960d65392
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-09-11 10:28:30 +0100

    ML-1488: LCML dashboard filter pane (#628)
    * ML-1488 lcml dashboard filter pane (#614)
    
    * Convert projects list to a post to be ready for search params
    
    * All filter for show
    
    * ML-1488: LCML dashboard filter status and type fields (#629)
    
    * Convert projects list to a post to be ready for search params
    
    * All filter for show
    
    * Status filter
    
    * Type filter
    
    * plural value update
    
    * Fix lint
    
    * ML-1488: lcml dashboard filter users (#630)
    
    * Convert projects list to a post to be ready for search params
    
    * All filter for show
    
    * Status filter
    
    * Type filter
    
    * plural value update
    
    * Fix lint
    
    * Specific user schema
    
    * schema and filter query
    
    * Return projects list
    
    * Fix missing logic
    
    * sonar update
    
    * Get user names from dynamics
    
    * Fix sonar issue
    
    ---------
    
    Co-authored-by: Vasudev Ganesan <vasudev.ganesan@esynergy.co.uk>

--- src/shared/api/projects/index.js
+++ src/shared/api/projects/index.js
@@ -1,9 +1,15 @@
 import { getProjectsController } from './controllers/get-projects.js'
+import { getUsersController } from './controllers/get-users.js'
 
 export const projects = [
   {
-    method: 'GET',
+    method: 'POST',
     path: '/projects',
     ...getProjectsController
+  },
+  {
+    method: 'POST',
+    path: '/projects/users',
+    ...getUsersController
   }
 ]
